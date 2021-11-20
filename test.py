519d2b62564fbebdcc6ad707ee6e6c4ae39de503
santy81855 <mynameissantiagogarcia@gmail.com>
2021-11-19 20:02:01 -0500

    progress

--- test.py
+++ test.py
@@ -21,6 +21,17 @@ import os
 import subprocess
 from pathlib import Path
 import ctypes
+#
+import win32api
+import win32gui
+
+from ctypes.wintypes import LONG
+
+from win32con import WM_NCCALCSIZE, GWL_STYLE, WM_NCHITTEST, WS_MAXIMIZEBOX, WS_THICKFRAME, \
+    WS_CAPTION, HTTOPLEFT, HTBOTTOMRIGHT, HTTOPRIGHT, HTBOTTOMLEFT, \
+    HTTOP, HTBOTTOM, HTLEFT, HTRIGHT, HTCAPTION, WS_POPUP, WS_SYSMENU, WS_MINIMIZEBOX
+from PyQt5.QtWinExtras import QtWin
+#
 
 class QLineNumberArea(QWidget):
     def __init__(self, editor):
@@ -294,6 +305,7 @@ class Tab(QWidget):
         titleBar.title.setText(self.fileName)
         
 class MainWindow(QWidget):
+    BORDER_WIDTH = 5
     def __init__(self):
         super(MainWindow, self).__init__()
         self.keyPressEvent = self.keyPressEvent
@@ -431,6 +443,80 @@ class MainWindow(QWidget):
         self.textbox.textChanged.connect(self.setSavedToFalse)
         # to help rounded corners work
         self.setAttribute(Qt.WA_TranslucentBackground)
+        #-----------------------------------------------------------------------------------------------------------------------------------#
+        self.hwnd = self.winId().__int__()
+        window_style = win32gui.GetWindowLong(self.hwnd, GWL_STYLE)
+        win32gui.SetWindowLong(self.hwnd, GWL_STYLE, window_style | WS_POPUP | WS_THICKFRAME | WS_CAPTION | WS_SYSMENU
+                                                                  | WS_MAXIMIZEBOX | WS_MINIMIZEBOX)
+
+        if QtWin.isCompositionEnabled():
+            # Aero Shadow
+            QtWin.extendFrameIntoClientArea(self, -1, -1, -1, -1)
+        else:
+            QtWin.resetExtendedFrame(self)
+    def changeEvent(self, event):
+        if event.type() == event.WindowStateChange:
+            if self.windowState() & Qt.WindowMaximized:
+                margin = abs(self.mapToGlobal(self.rect().topLeft()).y())
+                self.setContentsMargins(margin, margin, margin, margin)
+            else:
+                self.setContentsMargins(0, 0, 0, 0)
+
+        return super(MainWindow, self).changeEvent(event)
+
+    def nativeEvent(self, event, message):
+        return_value, result = super().nativeEvent(event, message)
+
+        # if you use Windows OS
+        if event == b'windows_generic_MSG':
+            msg = ctypes.wintypes.MSG.from_address(message.__int__())
+            # Get the coordinates when the mouse moves.
+            x = win32api.LOWORD(LONG(msg.lParam).value)
+            # converted an unsigned int to int (for dual monitor issue)
+            if x & 32768: x = x | -65536
+            y = win32api.HIWORD(LONG(msg.lParam).value)
+            if y & 32768: y = y | -65536
+
+            x -= self.frameGeometry().x()
+            y -= self.frameGeometry().y()
+
+            # Determine whether there are other controls(i.e. widgets etc.) at the mouse position.
+            if self.childAt(x, y) is not None and self.childAt(x, y) is not self.findChild(QWidget, "ControlWidget"):
+                # passing
+                if self.width() - self.BORDER_WIDTH > x > self.BORDER_WIDTH and y < self.height() - self.BORDER_WIDTH:
+                    return return_value, result
+
+            if msg.message == WM_NCCALCSIZE:
+                # Remove system title
+                return True, 0
+
+            if msg.message == WM_NCHITTEST:
+                w, h = self.width(), self.height()
+                lx = x < self.BORDER_WIDTH
+                rx = x > w - self.BORDER_WIDTH
+                ty = y < self.BORDER_WIDTH
+                by = y > h - self.BORDER_WIDTH
+                if lx and ty:
+                    return True, HTTOPLEFT
+                if rx and by:
+                    return True, HTBOTTOMRIGHT
+                if rx and ty:
+                    return True, HTTOPRIGHT
+                if lx and by:
+                    return True, HTBOTTOMLEFT
+                if ty:
+                    return True, HTTOP
+                if by:
+                    return True, HTBOTTOM
+                if lx:
+                    return True, HTLEFT
+                if rx:
+                    return True, HTRIGHT
+                # Title
+                return True, HTCAPTION
+
+        return return_value, result
+        #-----------------------------------------------------------------------------------------------------------------------------------#
 
 
     def paintEvent(self, ev):
@@ -973,9 +1059,10 @@ class MyBar(QWidget):
             }
             QPushButton::hover
             {
-                background-color : #990000;
+            background-color : #990000;
             }
                                 """)
+                                
         self.btn_close.setMouseTracking(True)
 
         self.btn_min = QPushButton("-")
